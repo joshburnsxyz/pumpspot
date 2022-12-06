933f6c82988aa5e9c2cb68b8325c9ab63427b4f6
Josh Burns <joshyburnss@gmail.com>
2022-12-07 09:24:23 +1100

    bundle javascript and init leaflet map

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
 import resolve from "@rollup/plugin-node-resolve"
 
-export default {
-  input: "app/javascript/application.js",
-  output: {
-    file: "app/assets/builds/application.js",
-    format: "es",
-    inlineDynamicImports: true,
-    sourcemap: true
+export default [
+  {
+    input: "app/javascript/application.js",
+    output: {
+      file: "app/assets/builds/application.js",
+      format: "es",
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    plugins: [
+      resolve()
+    ]
   },
-  plugins: [
-    resolve()
-  ]
-}
+  {
+    input: "app/javascript/map.js",
+    output: {
+      file: "app/assets/builds/map.js",
+      format: "es",
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    plugins: [
+      resolve()
+    ]
+  }
+
+]
