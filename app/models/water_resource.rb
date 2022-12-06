dcad964cb840e36b99f633744ef1639e92b824c9
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-06 01:31:36 +0000

    validate data params

--- app/models/water_resource.rb
+++ app/models/water_resource.rb
@@ -1,4 +1,15 @@
 class WaterResource < ApplicationRecord
 
   enum :category, %i(small_tank, medium_tank, large_tank, pond, reservoir, lake, river, hydrant, other)
+
+  # Ensure data is present
+  validates :title, presence: true
+  validates :capacity, presence: true
+  validates :gps_lon, presence: true
+  validates :gps_lat, presence: true
+  validates :designation, presence: true
+  validates :area, presence: true
+  validates :surface_elevation, presence: true
+
+  
 end
