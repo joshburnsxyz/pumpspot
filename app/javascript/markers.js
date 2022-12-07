0dc43f841f9942d6840592bae1f32428f79e7d4a
Josh Burns <joshyburnss@gmail.com>
2022-12-07 22:40:39 +1100

    open popup

--- app/javascript/markers.js
+++ app/javascript/markers.js
@@ -1,10 +1,9 @@
 function createMarker(name,L,lat,lon) {
   var marker = L.marker([lat,lon]).addTo(map);
-  marker.bindPopup("<b>"+name+"</b><hr/>Capacity: "+r.capacity+"L");
+  marker.bindPopup("<b>"+name+"</b><hr/>Capacity: "+r.capacity+"L").openPopup();
 }
 
 for (i = 0; i < resources.length; i++) {
-  console.log("hi");
   r = resources[i];
   createMarker(r.title,L,r.gps_lat,r.gps_lon);
 }
