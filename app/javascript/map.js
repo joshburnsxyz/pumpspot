2c7344e63bd6138771a69a9e35ad64d072f04a41
Josh Burns <joshyburnss@gmail.com>
2022-12-07 22:48:40 +1100

    load extra map assets from CDN

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,6 +1,6 @@
 import 'leaflet';
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
