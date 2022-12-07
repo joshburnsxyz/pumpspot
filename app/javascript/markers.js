766c0d158add43c3db6f9b37a4cd7da77e8608f5
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-07 19:55:46 +0000

    add water type to info popup

--- app/javascript/markers.js
+++ app/javascript/markers.js
@@ -1,9 +1,14 @@
-function createMarker(name,L,lat,lon) {
-  var marker = L.marker([lat,lon]).addTo(map);
-  marker.bindPopup("<b>"+name+"</b><hr/>Capacity: "+r.capacity+"L").openPopup();
+function createMarker(L,r) {
+  var marker = L.marker([r.lat,r.lon]).addTo(map);
+  if (r.salt_water == true) {
+    var waterType = "Salt Water"
+  } else {
+    var waterType = "Fresh Water"
+  }
+  marker.bindPopup("<b>"+r.title+"</b><hr/>Capacity: "+r.capacity+"L<br/>"+waterType).openPopup();
 }
 
 for (i = 0; i < resources.length; i++) {
   r = resources[i];
-  createMarker(r.title,L,r.gps_lat,r.gps_lon);
+  createMarker(r.title,L,r);
 }
