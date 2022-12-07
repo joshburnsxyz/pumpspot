9a1078d43a3f4b7535949a1cd95a16819f10bccf
Josh Burns <joshyburnss@gmail.com>
2022-12-07 17:06:36 +1100

    filter by name

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,9 @@
 class MapController < ApplicationController
   def index
+    if params["name"] != nil
+      cat = params["name"]
+      @q = WaterResource.filter_by_name(cat)
+    end
     if params["cat"] != nil
       cat = params["cat"]
       @q = WaterResource.filter_by_category(cat)
